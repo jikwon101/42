5c92ddda346b0979b4bc0ef4c948799403e6c843
jieun kwon <jieunkwon@jieunui-MacBookPro.local>
2021-03-04 22:24:32 +0900

    .

--- philosopher/philo_two/clean.c
+++ philosopher/philo_two/clean.c
@@ -6,7 +6,7 @@
 /*   By: jikwon <marvin@42.fr>                      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/03/04 13:40:29 by jikwon            #+#    #+#             */
-/*   Updated: 2021/03/04 18:05:01 by jikwon           ###   ########.fr       */
+/*   Updated: 2021/03/04 22:24:06 by jikwon           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,8 +17,5 @@ int		clean_sema(int code)
 	sem_unlink("s_fork");
 	sem_unlink("s_write");
 	sem_unlink("s_state");
-	sem_close(g_info.s_fork);
-	sem_close(g_info.s_write);
-	sem_close(g_info.s_state);
 	return (code);
 }
