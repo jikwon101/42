985d1ac74f1da699d2a3a47c72f224cc6ad92b4c
jieun kwon <jieunkwon@jieunui-MacBookPro.local>
2021-04-03 21:38:14 +0900

    .

--- cpp/c06/ex00/Conversion.cpp
+++ cpp/c06/ex00/Conversion.cpp
@@ -1,12 +1,14 @@
 #include "Conversion.hpp"
 
+/*
+** Coplien's Form
+*/
 Conversion::Conversion(std::string raw)
 {
 	this->raw = raw;
-	this->fraction = 1;
-	this->sign = 1;
+	this->fraction = (raw.find(".") == std::string::npos) ? 1 : 2;
+	this->sign = (raw.find("-") == std::string::npos) ? 1 : -1;
 	type = detectType();
-	fraction = 1;
 	if (type == CHAR)
 	{
 		ival = static_cast<int64_t>(raw.c_str()[0]);
@@ -14,37 +16,46 @@ Conversion::Conversion(std::string raw)
 	}
 	else
 	{
-		dval = toDval();
+		dval = atof(raw.c_str());
 		ival = static_cast<int64_t>(dval);
 	}
 }
 
-double	Conversion::toDval()
-{
-	size_t		pos_dot;
-	double		res = 0;
+Conversion::~Conversion(){}
 
-	if (type != FLOAT && type != DOUBLE && type != CHAR && type != INT)
-		return (0);
-	res = atof(raw.c_str());
-	pos_dot = raw.find(".");
-	if (pos_dot != std::string::npos)
-		this->fraction = 2;
-	if (raw.find("-") != std::string::npos)
-		this->sign = -1;
-	return (res);
+Conversion::Conversion(Conversion const & src)
+{
+	raw = src.raw;
+	ival = src.ival;
+	dval = src.dval;
+	type = src.type;
+	fraction = src.fraction;
+	sign = src.sign;
 }
 
+Conversion& Conversion::operator=(Conversion const & src)
+{
+	if (this != &src)
+	{
+		raw = src.raw;
+		ival = src.ival;
+		dval = src.dval;
+		type = src.type;
+		fraction = src.fraction;
+		sign = src.sign;
+	}
+	return (*this);
+}
 
+/*
+** Print Function : Public
+*/
 void	Conversion::printAsChar(std::ostream & os) const
 {
 	os << "char: ";
 	if (type == N || type == IMPOSSIBLE || type == POSINF || type == NEGINF)
-	{
 		os << "impossible\n";
-		return ;
-	}
-	if ((ival ^ sign) < 0)
+	else if ((ival ^ sign) < 0)
 		os << "impossible\n";
 	else if (ival > 127)
 		os << "impossible\n";
@@ -97,14 +108,116 @@ void	Conversion::printAsFloat(std::ostream & os) const
 	else if (type == N)
 		os << "nanf\n";
 	else if (((int64_t)dval ^ (int64_t)sign) < 0)
-	{
 		os << "impossible\n";
-	}
 	else
 	{
 		os.precision(fraction);
 		os << std::fixed << static_cast<float>(dval);
 		os << "f" << std::endl;
 	}
+}
+
+/*
+** Overload ostream operator
+*/
+std::ostream& operator<<(std::ostream & os, Conversion const & src)
+{
+	src.printAsChar(os);
+	src.printAsInt(os);
+	src.printAsFloat(os);
+	src.printAsDouble(os);
+	return (os);
+}
+
+
+/*
+** Member Function : private
+*/
+int	Conversion::detectType()
+{
+	std::string	src;
+	size_t 		pos_dot;
+	size_t		pos_minus;
+	size_t		pos_eplus;
+
+	pos_minus = raw.find("-");
+	src = (pos_minus == std::string::npos) ? raw : raw.substr(1, raw.length() - 1);
+	pos_dot = src.find(".");
+	pos_eplus = src.find("e+");
+
+	if (pos_dot != std::string::npos || pos_eplus != std::string::npos)
+	{
+		if (isDecimalNbr(src, pos_dot, pos_eplus) == false)
+			return (IMPOSSIBLE);
+		else if (src.find("f") != std::string::npos)
+			return (FLOAT);
+		return (DOUBLE);
+	}
+	if (raw == "nan" || raw == "nanf")
+		return (N);
+	if (raw == "inf" || raw == "inff")
+		return (POSINF);
+	if (raw == "-inf" || raw == "-inff")
+		return (NEGINF);
+	if (raw.length() == 1 && (isInt(raw) == false))
+		return (CHAR);
+	if (isInt(src) == true)
+		return (INT);
+	return (IMPOSSIBLE);
+}
+
+bool	Conversion::isInt(std::string const & str)
+{
+	const char	*val;
+	int 		i;
+
+	i = 0;
+	val = str.c_str();
+	while (val[i])
+	{
+		if ('0' <= val[i] && val[i] <= '9')
+			i++;
+		else
+			return (false);
+	}
+	return (true);
+}
+
+bool Conversion::isDecimalNbr(std::string const & src, size_t pos_dot, size_t pos_eplus)
+{
+	size_t length;
+
+	if (src.find("f") == src.length() - 1)
+		length = src.length() - 1;
+	else
+		length = src.length();
+	if (pos_eplus == std::string::npos)
+	{
+		if (isInt(src.substr(0, pos_dot)) == false)
+			return (false);
+		if (pos_dot == length - 1)
+			return (true);
+		if (isInt(src.substr(pos_dot + 1, length - 1 - pos_dot)) == false)
+			return (false);
+	}
+	else if (pos_dot == std::string::npos)
+	{
+		if (isInt(src.substr(0, pos_eplus)) == false)
+			return (false);
+		if (isInt(src.substr(pos_eplus + 2, length - pos_eplus -2)) == false)
+			return (false);
+	}
+	else
+	{
+		if (pos_eplus < pos_dot)
+			return (false);
+		if (isInt(src.substr(0, pos_dot)) == false)
+			return (false);
+		if (isInt(src.substr(pos_dot + 1, pos_eplus - 1 - pos_dot - 1 + 1)) == false)
+			return (false);
+		if (isInt(src.substr(pos_eplus + 2, length - pos_eplus - 2)) == false)
+			return (false);
+	}
+	return (true);
 }
 
--- cpp/c06/ex00/Conversion.hpp
+++ cpp/c06/ex00/Conversion.hpp
@@ -1,23 +1,12 @@
 #ifndef CONVERSION_HPP
 # define CONVERSION_HPP
 
-#include <stdlib.h>
 #include <iostream>
 #include <string>
-#include <math.h>
-# define CL "\033[1m\033[36m"
-# define RS "\033[0m"
 
 class Conversion
 {
 private:
-	std::string raw;
-	int64_t		ival;
-	double		dval;
-	int			type;
-	int			fraction;
-	int			sign;
-public:
 	enum	Type
 	{
 		N,
@@ -29,19 +18,25 @@ public:
 		NEGINF,
 		IMPOSSIBLE
 	};
+	std::string raw;
+	int64_t		ival;
+	double		dval;
+	int			type;
+	int			fraction;
+	int			sign;
+	bool		isDecimalNbr(std::string const & src, size_t pos_dot, size_t pos_eplus);
+	bool		isInt(std::string const & str);
+	int			detectType();
+
+public:
 	Conversion(std::string src);
-//	~Conversion();
-//	Conversion(Conversion & const src);
-//	Conversion& operator=(Conversion & const src);
+	~Conversion();
+	Conversion(Conversion const & src);
+	Conversion& operator=(Conversion const & src);
 	void		printAsInt(std::ostream & os) const;
 	void		printAsDouble(std::ostream & os) const;
 	void		printAsFloat(std::ostream & os) const;
 	void		printAsChar(std::ostream & os) const;
-	int			getType() const;
-	int			detectType();
-	//std::string printtype();
-	//int			toIval();
-	double		toDval();
 };
 
 std::ostream& operator<<(std::ostream & os, Conversion const & src);
