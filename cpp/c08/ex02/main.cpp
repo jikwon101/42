55abd39646a45ca49be5b0277f9939f75cfa2662
jikwon <jikwon@student.42seoul.kr>
2021-04-11 18:44:33 +0900

    .

--- cpp/c08/ex02/main.cpp
+++ cpp/c08/ex02/main.cpp
@@ -6,11 +6,33 @@ class MutantStack :  public std::stack<T>
 {
 public:
 	MutantStack();
-	~MutantStack();
+	virtual ~MutantStack();
 	MutantStack(MutantStack<T> const & src);
 	MutantStack<T>& operator=(MutantStack<T> const & src);
+
+	typedef typename std::stack<T>::container_type::iterator iterator;
+	typedef typename std::stack<T>::container_type::const_iterator const_iterator;
+	typedef typename std::stack<T>::container_type::reverse_iterator reverse_iterator;
+	typedef typename std::stack<T>::container_type::const_reverse_iterator const_reverse_iterator;
+	iterator begin(void);
+	const_iterator begin(void) const;
+	reverse_iterator rbegin(void);
+	const_reverse_iterator rbegin(void) const;
+	iterator end(void);
+	const_iterator end(void);
+	reverse_iterator rend(void);
+	const_reverse_iterator rend(void) const;
 	
 };
+
+template <typename T>
+MutantStack<T>::MutantStack(){}
+template <typename T>
+MutantStack<T>::~MutantStack(){}
+template <typename T>
+MutantStack<T>::MutantStack(MutantStack<T> const & src){}
+template <typename T>
+MutantStack<T>& MutantStack<T>::operator=(MutantStack<T> const & src){}
 
 int main()
 {
@@ -20,7 +42,4 @@ int main()
 	my.push(2);
 	std::cout << my.top() << std::endl;
 	MutantStack<int>::iterator it;
-	it.b = 3;
-	std::cout << it.b << std::endl;
-
 }
