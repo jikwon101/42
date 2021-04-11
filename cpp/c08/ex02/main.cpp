cf750c9956e4c63c8dbf9f596e481de8d96d838d
jikwon <jikwon@student.42seoul.kr>
2021-04-11 21:04:07 +0900

    .

--- cpp/c08/ex02/main.cpp
+++ cpp/c08/ex02/main.cpp
@@ -19,7 +19,7 @@ public:
 	reverse_iterator rbegin(void);
 	const_reverse_iterator rbegin(void) const;
 	iterator end(void);
-	const_iterator end(void);
+	const_iterator end(void) const;
 	reverse_iterator rend(void);
 	const_reverse_iterator rend(void) const;
 	
@@ -33,6 +33,34 @@ template <typename T>
 MutantStack<T>::MutantStack(MutantStack<T> const & src){}
 template <typename T>
 MutantStack<T>& MutantStack<T>::operator=(MutantStack<T> const & src){}
+
+template <typename T>
+typename MutantStack<T>::iterator MutantStack<T>::begin(void)
+{return (this->c.begin());
+}
+template <typename T>
+typename MutantStack<T>::const_iterator MutantStack<T>::begin(void) const
+{return (this->c.begin());
+}
+template <typename T>
+typename MutantStack<T>::reverse_iterator MutantStack<T>::rbegin(void)
+{return (this->c.rbegin());
+}
+template <typename T>
+typename MutantStack<T>::const_reverse_iterator MutantStack<T>::rbegin(void) const
+{return (this->c.rbegin());}
+template <typename T>
+typename MutantStack<T>::iterator MutantStack<T>::end(void)
+{return (this->c.end());}
+template <typename T>
+typename MutantStack<T>::const_iterator MutantStack<T>::end(void) const
+{return (this->c.end());}
+template <typename T>
+typename MutantStack<T>::reverse_iterator MutantStack<T>::rend(void)
+{return (this->c.rend());}
+template <typename T>
+typename MutantStack<T>::const_reverse_iterator MutantStack<T>::rend(void) const
+{return (this->c.rend());}
 
 int main()
 {
