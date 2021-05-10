8d6c40c3398d29671b321af30a3636ebce804e81
jikwon <jikwon@student.42seoul.kr>
2021-05-10 11:45:37 +0900

    before execute testdiff.sh

--- container/list/tester/testdiff.sh
+++ container/list/tester/testdiff.sh
@@ -1,14 +1,18 @@
+# How to execute this file
+# list
+# in list root folder 
+# ./tester/testdiff.sh list
 
 # How to compile printdiff.exe
 # In tester folder
 # gcc main.c get_next_line.a -D PATH="\"$PWD\"" -o printdiff.exe
 
-$PWD/tester/replace.exe $PWD/main.cpp "ft::${1}" "std::${1}";
+./tester/replace.exe $PWD/main.cpp "ft::${1}" "std::${1}";
 mv $PWD/main.cpp.replace $PWD/tester/teststd.cpp;
-clang++ -o ft  $PWD/main.cpp;
-clang++ -o std $PWD/teststd.cpp -D TEST;
+clang++ -o ./tester/ft  $PWD/main.cpp;
+clang++ -o ./tester/std $PWD/tester/teststd.cpp -D TEST;
 echo  "\033[1m\033[36mResult of diff\n \033[0m";
-./ft > $PWD/tester/result_ft.txt
-./std > $PWD/tester/result_std.txt
-./$PWD/tester/printdiff.exe
+./tester/ft > $PWD/tester/result_ft.txt
+./tester/std > $PWD/tester/result_std.txt
+./tester/printdiff.exe
 #rm teststd.cpp
