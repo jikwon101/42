7c24db92b16703b2e0a795b7498960658bf791a8
jikwon <jikwon@student.42seoul.kr>
2021-06-07 15:10:38 +0900

    .

--- container/map/tester/teststd.cpp
+++ container/map/tester/teststd.cpp
@@ -6,105 +6,127 @@
 #include <map>
 #endif
 
-template <typename T, typename U>
-void	print(std::map<T, U> const& src);
+#define NL print("\n")
+
+template <typename X, typename T, typename U>
+void	print(X const& name, std::map<T, U> const& src);
 template <typename T>
-void	print(T src);
+void	print(T  src);
 template <typename T, typename U>
 void	print_itpair(T val, U end);
+template <typename T>
+void	print_it(T const& it, T const& end);
 
 int main()
 {
+	print("-------------------------\n");
+	print("CONSTRUCTOR/ITERATOR\n");
+	print("-------------------------\n");
 	{
 		int arr[] = {2,4,6,8,10,12,14};
 		std::map<int, int> m1;
 		for (int i = 0; i < 7 ; i++)
 			m1[arr[i]];
-		print("m1 : "); print(m1);
+		print("m1", m1);
 		std::map<int, int> m2(m1);
 		std::map<int, int> m3(m1.begin(), m1.begin());
 		std::map<int, int> m4(m1.begin(), m1.end());
 		std::map<int, int> m5;
 		m5 = m1;
-		print("m2 : "); print(m2);
-		print("m3 : "); print(m3);
-		print("m4 : "); print(m4);
-		print("m5 : "); print(m5);
-		std::cout << "m5 reverse print : \n";
+		print("m2", m2);
+		print("m3", m3);
+		print("m4", m4);
+		print("m5", m5);
+		print("m5 reverse print : \n");
 		for (std::map<int, int>::reverse_iterator it = m5.rbegin() ; it != m5.rend() ; ++it)
 		{
 			std::cout << (*it).first << " ";
 		}
-		std::cout << "\n";
+		NL;
 		std::map<int, int>::iterator it = m1.begin();
 		std::map<int, int>::const_iterator cit(it);
 		cit = it;
 		if (cit == it) print("cit == it\n"); else print("cit != it\n");
 
 	}
+	NL;
+	print("-------------------------\n");
+	print("CAPACITY\n");
 	print("-------------------------\n");
 	{
 		std::map<int, int> m1;
-		print("m1 is empty : "); print(m1.empty()); print("\n");
-		print("m1 size : "); print(m1.size()); print("\n");
+		print("m1 is empty : "); print(m1.empty()); NL;
+		print("m1 size : "); print(m1.size()); NL;
 		m1[10];
-		print("key10, value : "); print(m1[10]); print("\n");
+		print("key10, value : "); print(m1[10]); NL;
 		m1[10] = 20;
-		print("key10, value : "); print(m1[10]); print("\n");
+		print("key10, value : "); print(m1[10]); NL;
 		m1[100] = 100; m1[200] = 200;
-		print(m1);
-		print("m1 is empty : "); print(m1.empty()); print("\n");
-		print("m1 size : "); print(m1.size()); print("\n");
-		print("m1 max : "); print(m1.max_size()); print("\n");
+		print("m1", m1);
+		print("m1 is empty : "); print(m1.empty()); NL;
+		print("m1 size : "); print(m1.size()); NL;
+		print("m1 max : "); print(m1.max_size()); NL;
 	}
+	NL;
 	print("-------------------------\n");
-	{
-		std::pair<int, int> t1(3, 10);
-		std::pair<std::map<int, int>::iterator, bool> res;
-		std::map<int, int> m1;
-		res = m1.insert(t1);
-		std::cout << "pair : (" << (*(res.first)).first << " , " << (*(res.first)).second << ")" << ", res : " << res.second << "\n";
-		res = m1.insert(t1);
-		std::cout << "pair : (" << (*(res.first)).first << " , " << (*(res.first)).second << ")" << ", res : " << res.second << "\n";
-
-		m1[4]; m1[10]; m1[1]; m1[0]; m1[8];
-		std::map<int, int>::iterator it;
-		std::pair<int, int> t2(2, 22);
-		it = m1.insert(m1.find(10), t2);
-		std::cout << "pair : (" << (*it).first << " , " << (*it).second << ")\n";
-		std::map<int, int> m2;
-		print("m1"); print(m1); print("\n");
-		print("m2: insert\n");m2.insert(m1.begin(), m1.end());
-		print("m2"); print(m2);
-	}
+	print("MODIFIERS\n");
 	print("-------------------------\n");
 	{
 		int arr[] = {1,9,4,6,7,8,2,4,0,10};
 		std::map<int, int> m1;
 		for ( int n = 0 ; n < 10 ; n++)
 			m1[arr[n]];
-		print(m1);
+		print("m1", m1);
 		m1.erase(m1.begin());
-		print(m1);
-		int res = m1.erase(4);
+		print("m1", m1);
+		print("erase 4\n"); int res = m1.erase(4);
 		print("res : "); print(res); print("\n");
-		res = m1.erase(14);
+		print("erase 14\n"); res = m1.erase(14);
 		print("res : "); print(res); print("\n");
-		print(m1);
-		m1.erase(m1.find(2), m1.find(8));
-		print(m1);
+		print("m1", m1);
+		print("erase 2-8\n"); m1.erase(m1.find(2), m1.find(8));
+		print("m1", m1);
 	
 		std::map<int, int> m2;
-		print("m1: "); print(m1);
-		print("m2: "); print(m2);
+		print("m1", m1);
+		print("m2", m2);
 		print("m2 swap m1\n"); m2.swap(m1);
-		print("m1: "); print(m1);
-		print("m2: "); print(m2);
+		print("m1", m1);
+		print("m2", m2);
 		print("m2 clear\n"); m2.clear();
-		print("m2: "); print(m2);
+		print("m2", m2);
 		m2[10] = 100;
-		print("m2: "); print(m2);
+		print("m2", m2);
 	}
+
+	NL;
+	print("-------------------------\n");
+	print("MODIFIERS - INSERT\n");
+	print("-------------------------\n");
+	{
+		std::pair<int, int> t1(3, 10);
+		std::pair<std::map<int, int>::iterator, bool> res;
+		std::map<int, int> m1;
+		print("insert (3,10)\n"); res = m1.insert(t1);
+		print("res : "); print(res.second); print(" ");
+		print_it(res.first, m1.end()); NL;
+		print("insert (3,10)\n"); res = m1.insert(t1);
+		print("res : "); print(res.second); print(" ");
+		print_it(res.first, m1.end()); NL;
+
+		m1[4]; m1[10]; m1[1]; m1[0]; m1[8];
+		std::map<int, int>::iterator it;
+		std::pair<int, int> t2(2, 22);
+		it = m1.insert(m1.find(10), t2);
+		print_it(it, m1.end()); NL;
+		std::map<int, int> m2;
+		print("m1", m1);
+		print("m2: insert\n");m2.insert(m1.begin(), m1.end());
+		print("m2", m2);
+	}
+	NL;
+	print("-------------------------\n");
+	print("OPEATIONS\n");
 	print("-------------------------\n");
 	{
 		std::map<int, int> m1;
@@ -114,13 +136,14 @@ int main()
 		std::map<int, int>::iterator res;
 		print("count 4 : "); print(m1.count(4)); print("\n");
 		print("find 4\n");res = m1.find(4);
-		print("res : "); print((*res).first); print("\n");
-	
+		print("res : "); print_it(res, m1.end()); NL;
 		print("count 20 : "); print(m1.count(20)); print("\n");
 		print("find 20\n"); res = m1.find(20);
-		if (res == m1.end())
-			print("res is pointing end\n");
+		print("res : "); print_it(res, m1.end()); NL;
 	}
+	NL;
+	print("-------------------------\n");
+	print("BOUND\n");
 	print("-------------------------\n");
 	{	
 		std::map<int, int> m1;
@@ -128,56 +151,23 @@ int main()
 		for ( int n = 0 ; n < 7 ; n++)
 			m1[arr[n]];
 		std::map<int, int>::iterator it;
-		print(m1);
+		print("m1", m1);
 		print("lower(0) :"); it = m1.lower_bound(0);
-		if (it != m1.end())
-		{	print((*it).first); print("\n");}
-		else
-			print("end\n");
+		print_it(it, m1.end()); NL;
 		print("lower(2) :"); it = m1.lower_bound(2);
-		if (it != m1.end())
-		{	print((*it).first); print("\n");}
-		else
-			print("end\n");
+		print_it(it, m1.end()); NL;
 		print("lower(5) :"); it = m1.lower_bound(5);
-		if (it != m1.end())
-		{	print((*it).first); print("\n");}
-		else
-			print("end\n");
+		print_it(it, m1.end()); NL;
 		print("lower(20) :"); it = m1.lower_bound(20);
-		if (it != m1.end())
-		{	print((*it).first); print("\n");}
-		else
-			print("end\n");
-	}
-	print("-------------------------\n");
-	{	
-		std::map<int, int> m1;
-		int arr[] = {2,4,6,8,10,12, 14};
-		for ( int n = 0 ; n < 7 ; n++)
-			m1[arr[n]];
-		std::map<int, int>::iterator it;
-		print(m1);
+		print_it(it, m1.end()); NL;
 		print("upper(0) :"); it = m1.upper_bound(0);
-		if (it != m1.end())
-		{	print((*it).first); print("\n");}
-		else
-			print("end\n");
+		print_it(it, m1.end()); NL;
 		print("upper(2) :"); it = m1.upper_bound(2);
-		if (it != m1.end())
-		{	print((*it).first); print("\n");}
-		else
-			print("end\n");
+		print_it(it, m1.end()); NL;
 		print("upper(5) :"); it = m1.upper_bound(5);
-		if (it != m1.end())
-		{	print((*it).first); print("\n");}
-		else
-			print("end\n");
+		print_it(it, m1.end()); NL;
 		print("upper(20) :"); it = m1.upper_bound(20);
-		if (it != m1.end())
-		{	print((*it).first); print("\n");}
-		else
-			print("end\n");
+		print_it(it, m1.end()); NL;
 	}
 	print("-------------------------\n");
 	{
@@ -186,7 +176,7 @@ int main()
 		int arr[] = {2,4,6,8,10,12, 14};
 		for ( int n = 0 ; n < 7 ; n++)
 			m1[arr[n]];
-		print("m1 :"); print(m1);
+		print("m1", m1);
 		print("range(0)");
 		res = m1.equal_range(0);
 		print_itpair(res, m1.end());
@@ -200,16 +190,19 @@ int main()
 		res = m1.equal_range(20);
 		print_itpair(res, m1.end());
 	}
-	std::cout << "-------------\n";
+	NL;
+	print("-------------------------\n");
+	print("RELATIONAL OPERATORS\n");
+	print("-------------------------\n");
 	{
 		std::map<int, int> t1;
 		t1[10] = 20;
 		std::map<int, int> t2(t1);
 		std::map<int, int> t3;
 		t3[10] = 30;
-		print("t1 : "); print(t1);
-		print("t2 : "); print(t2);
-		print("t3 : "); print(t3);
+		print("t1", t1);
+		print("t2", t2);
+		print("t3", t3);
 		print("t1 == t2  ? ");
 		if (t1 == t2) print("True\n") ; else print( "False\n");
 		print("t1 == t3  ? ");
@@ -260,11 +253,12 @@ std::ostream& operator<<(std::ostream& os, test & x)
 	os << x.nbr;
 	return (os);
 }
-template <typename T, typename U>
-void	print(std::map<T, U> const& src)
+template <typename X, typename T, typename U>
+void	print(X const& name, std::map<T, U> const& src)
 {
 	typename std::map<T, U>::const_iterator it;
 	
+	std::cout << name << ": ";
 	std::cout << "< ";
 	for (it = src.begin() ; it != src.end() ; ++it)
 	{
@@ -282,15 +276,19 @@ void print(T src)
 template <typename T, typename U>
 void	print_itpair(T val, U end)
 {
-	print("( ");
-	if (val.first == end)
-		print("end");
-	else
-		print((*(val.first)).first);
-	print(" , ");
-	if (val.second == end)
-		print("end");
-	else
-		print((*(val.second)).first);
-	std::cout << " )\n";
+	std::cout << "[ (";
+	print_it(val.first, end);
+	std::cout << ") , (";
+	print_it(val.second, end);
+	std::cout << ") )\n";
 }
+
+template <typename T>
+void	print_it(T const& it, T const& end)
+{
+	if (it == end)
+		std::cout << "end";
+	else
+		std::cout << (*it).first << " , " << (*it).second;
+}
+
