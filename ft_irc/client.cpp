97e073bafb4afa29688f3ebb102cb108894752c5
jikwon <jikwon@student.42seoul.kr>
2021-06-07 23:34:09 +0900

    .

--- ft_irc/client.cpp
+++ ft_irc/client.cpp
@@ -8,14 +8,37 @@
 #include <fcntl.h>
 #include <stdlib.h>
 #include <string>
+#include <unistd.h>
+#include <stdlib.h>
+#include <thread>
 
 #define SA sockaddr
 
 template <typename T>
 void print(T src) { std::cout << src; }
 
-int main()
+void funt(int fd)
+{	
+	int client_sockfd = fd;
+	int len = 100;
+	char rbuf[len];
+	int res = 0;
+
+	while (1)
+	{
+		if ((res = recv(client_sockfd, rbuf, len, 0)) == -1)
+		{ print("Error in recv\n");	}
+		else if (res > 0)
+		{
+			rbuf[res] = '\0';
+			std::cout << rbuf << "\n";
+		}
+	}
+}
+
+int main(int ac, char *av[])
 {
+
 	int server_sockfd, client_sockfd;
 	struct sockaddr_in serveraddr, clientaddr;
 	int res;
@@ -26,20 +49,22 @@ int main()
 	memset(&clientaddr, 0, sizeof(clientaddr));	
 	serveraddr.sin_family = PF_INET;
 	serveraddr.sin_addr.s_addr = inet_addr("127.0.0.1");	
-	serveraddr.sin_port = htons(8080);
+	serveraddr.sin_port = htons(80);
+	
 
 	if (connect(client_sockfd, (SA *)(&serveraddr),server) < 0)
 	{ print("Error in connect\n"); exit(1);}
 
-	char buf[1024];
-	int len = 1024;
+	std::thread _t1(funt, client_sockfd);
+	int len = 100;
+	//char sbuf[len];
+	std::string sbuf;
 	while (1)
 	{
-		if ((res = recv(client_sockfd, buf, len, 0)) == -1)
-		{ print("Error\n");	}
-		if (res)
-			std::cout << buf << std::endl;
-	}	
+		getline(std::cin, sbuf);
+		if ((res = send(client_sockfd, sbuf.c_str(), len, 0)) == -1)
+			print("Error in send\n");
+	}
 	close(client_sockfd);
 	exit(0);
 }
--- ft_irc/server.cpp
+++ ft_irc/server.cpp
@@ -7,6 +7,8 @@
 #include <unistd.h>
 #include <fcntl.h>
 #include <stdlib.h>
+#include <sys/seletc.h>
+#include <errno.hpp>
 
 #define SA sockaddr
 
@@ -15,58 +17,50 @@ void print(T src) { std::cout << src; }
 
 int main()
 {
+	fd_set sockets;
 	int server_sockfd, client_sockfd;
 	struct sockaddr_in serveraddr, clientaddr;
 	int res;
 	unsigned int client = sizeof(clientaddr);
 	int opt = 1;
 
-	/* Create socket */
 	server_sockfd = socket(PF_INET, SOCK_STREAM, IPPROTO_TCP);
-	/* Set socket option */
 	setsockopt(server_sockfd, SOL_SOCKET, SO_REUSEADDR, &opt, sizeof(opt));	
 	memset(&serveraddr, 0, sizeof(serveraddr));	
 	serveraddr.sin_family = PF_INET;
 	serveraddr.sin_addr.s_addr = htonl(INADDR_ANY);	
 	serveraddr.sin_port = htons(8080);
 
-	/* Binding */
 	if (bind(server_sockfd, (SA *)(&serveraddr), sizeof(serveraddr)) == -1)
 	{	print("Error in bind\n");  exit(1);}
 
-	/* Listenging */
 	if (listen(server_sockfd, 5) < 0)
 	{	print("Error in listen\n"); exit(1);}
+	
 
-	int buffersize = 1024;
-	char msg[buffersize];
-	int msgsize;
-	int set[10];
-	int cnt = 0;
 	print("Server is waiting\n");
 	while (1)
 	{
-
-	client_sockfd = accept(server_sockfd, (SA *)(&clientaddr), &client);
-	if (client_sockfd == -1)
-	{	print("Error in accept\n"); }
-	else
-		cnt++;
-	std::cout << client_sockfd << "is connected\n";
-	set[cnt-1] = client_sockfd;
-	print("Server is connected\n");
-	print("client ip : "); print(inet_ntoa(clientaddr.sin_addr)); print("\n");
-	char buf[] = "Connect is alive\n";
-	int len = strlen(buf);
-	for (int i = 0; i < cnt ; i++)
-	{
-		std::cout << set[i] << "trynig\n";
+		FD_SET(server_sockfd, &sockets);
+		FD_SET( ???, &sockets);
+		select();
+		if (FD_ISSET(server_sockfd, fd_set))
+		{
+			client_sockfd = accept(server_sockfd, (SA *)(&clientaddr), &client);
+			if (client_sockfd == -1)
+				print("Error in accept\n");
+			std::cout << client_sockfd << "is connected\n";
+			print("client ip : "); 
+			print(inet_ntoa(clientaddr.sin_addr)); print("\n");
+		}
+		for ()
+		{
+			if (FD_ISSET(s, fd_set))
+				do_client_service(s);
+		}
 		if (send(set[i], buf, len, 0) != -1)
 		{
 			std::cout << "Error in send\n";
 		}
 	}
-	}
-	//close(client_sockfd);
-	//close(server_sockfd);
 }
