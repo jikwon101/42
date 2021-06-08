2263b3b277ee472996d60569efe2dd0ffa448524
jikwon <jikwon@student.42seoul.kr>
2021-06-08 09:33:49 +0900

    .

--- ft_irc/bircd/client_read.c
+++ ft_irc/bircd/client_read.c
@@ -8,8 +8,10 @@ void	client_read(t_env *e, int cs)
 {
   int	r;
   int	i;
+  int temp;
 
   r = recv(cs, e->fds[cs].buf_read, BUF_SIZE, 0);
+  printf("recevie %d from User %d \n", r, cs);
   if (r <= 0)
     {
       close(cs);
@@ -21,9 +23,20 @@ void	client_read(t_env *e, int cs)
       i = 0;
       while (i < e->maxfd)
 	  {
+		char buf[1024] = {0,};
+		char num = cs + '0';
+		strcat(buf, "User ");
+		strcat(buf, &num);
+		strcat(buf, " : ");
+		strcat(buf, e->fds[cs].buf_read);
+		r += 9;
 	 	 if ((e->fds[i].type == FD_CLIENT) &&
 	  			 (i != cs))
-	   		send(i, e->fds[cs].buf_read, r, 0);
+	  	{
+			temp = send(i, buf, r, 0);
+			//temp = send(i, e->fds[cs].buf_read, r, 0);
+			printf("sent %dbytes to User %d\n", temp, i);
+		}
 		  i++;
 		}
     }
