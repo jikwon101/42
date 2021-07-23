abf25d19080171e295bf540c0d048fe3c6045874
jikwon <jikwon@student.42seoul.kr>
2021-07-24 01:41:11 +0900

    norm utils.*

--- push_swap/utils.h
+++ push_swap/utils.h
@@ -1,9 +1,9 @@
 #ifndef UTILS_H
 # define UTILS_H
 
-#include "libft.h"
-#include <stdlib.h>
-#include <unistd.h>
+# include "libft.h"
+# include <stdlib.h>
+# include <unistd.h>
 
 void	error(char *str);
 
