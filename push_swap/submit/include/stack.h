2e1b494e9fe973df360d061d4f58465dfae6b988
jikwon <jikwon@student.42seoul.kr>
2021-07-25 22:30:36 +0900

    .

--- push_swap/submit/include/stack.h
+++ push_swap/submit/include/stack.h
@@ -6,7 +6,7 @@
 /*   By: jikwon <jikwon@student.42seoul.kr>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/07/25 02:31:29 by jikwon            #+#    #+#             */
-/*   Updated: 2021/07/25 02:31:30 by jikwon           ###   ########.fr       */
+/*   Updated: 2021/07/25 22:13:30 by jikwon           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -25,7 +25,7 @@ typedef struct s_node
 
 typedef struct s_stack
 {
-	t_node	*head;		//last added data
+	t_node	*head;
 	int		size;
 }				t_stack;			
 
@@ -43,5 +43,4 @@ void	swap(t_stack *st);
 void	rotate(t_stack *st);
 void	reverse_rotate(t_stack *st);
 
-# include "stack_print.h" //temp
 #endif
